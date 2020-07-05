dc3aa7fdbfb55beb872c8a2b67b163b654645421
TPAMarvin <31587596+TPAMarvin@users.noreply.github.com>
2020-07-05 11:34:59 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+let angle = 0
+let millig = 0
+pins.servoWritePin(AnalogPin.P0, 90)
 basic.forever(function () {
-	
+    millig = input.acceleration(Dimension.X)
+    angle = pins.map(
+    millig,
+    -1023,
+    1023,
+    0,
+    180
+    )
+    pins.servoWritePin(AnalogPin.P0, angle)
 })
